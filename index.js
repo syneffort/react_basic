20b477fe7ea22492fb63e2c5fa48448e9c628b62
syneffort <syneffort@gmail.com>
2021-07-12 23:49:14 +0900

    security setting

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
 
 dotenv.config();
+
+const User = require('./models/User');
+const config = require('./config/key');
+
 const app = express();
 const port = 5000;
 
-mongoose.connect(process.env.MONGO_CONN, {
+
+app.use(bodyParser.urlencoded({ extended: true })); // application/x-www-form-urlencoded
+app.use(bodyParser.json()); // application/json
+
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -15,6 +24,19 @@ mongoose.connect(process.env.MONGO_CONN, {
     console.log('MongoDB connected...');
 }).catch((error) => {
     console.error(`❗ ${error}`);
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello Node!');
+});
+
+app.post('/register', (req, res) => {
+
+    const user = new User(req.body);
+    user.save((err, doc) => {
+        if (err) return res.json({ success: false, err });
+        return res.status(200).json({ success: true });
+    });
 });
 
 app.listen(port, () => {
